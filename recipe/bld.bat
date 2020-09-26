1b2f100b867d5ea1f95f6d01337abba30995d34b
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2020-09-26 19:26:26 +0100

    Bump to 1.66.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -30,7 +30,7 @@ set "PYTHONIOENCODING=UTF-8"
 @REM fix the env python path to match).
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%PYTHON%"') DO set "PYTHON_M=%%i"
 
-%PYTHON% %PREFIX%\Scripts\meson --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dcairo=enabled -Dcairo-libname=cairo-gobject.dll -Dpython=%PYTHON_M% ..
+%PYTHON% %PREFIX%\Scripts\meson --buildtype=release --prefix=%LIBRARY_PREFIX_M% --wrap-mode=nofallback --backend=ninja -Dcairo=enabled -Dcairo-libname=cairo-gobject.dll -Dpython=%PYTHON_M% ..
 if errorlevel 1 exit 1
 
 ninja -v
