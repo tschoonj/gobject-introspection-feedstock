bba93106ecdfb9d13b1d4488daad80665eea378a
Isuru Fernando <isuruf@gmail.com>
2020-11-14 22:15:33 -0600

    Fix script used

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,11 +19,14 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
     pushd build-host
 
     export CC=$CC_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
     export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
     export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
 
     # Unset them as we're ok with builds that are either slow or non-portable
     unset CFLAGS
+    unset CPPFLAGS
 
     $BUILD_PREFIX/bin/python3 $(which meson) --buildtype=release --prefix="$BUILD_PREFIX" --backend=ninja -Dlibdir=lib \
           -Dcairo=enabled -Dpython="$BUILD_PREFIX/bin/python3" ..
@@ -31,13 +34,13 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
     # This script would generate the functions.txt and dump.xml and save them
     # This is loaded in the native build. We assume that the functions exported
     # by glib are the same for the native and cross builds
-    export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-load.sh
-    ninja
+    export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-save.sh
+    ninja -j${CPU_COUNT}
     ninja install
 
     popd
   )
-  export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-save.sh
+  export GI_CROSS_LAUNCHER=$PREFIX/libexec/gi-cross-launcher-load.sh
   MESON_ARGS="-Dgi_cross_use_prebuilt_gi=True ${MESON_ARGS}"
 fi
 
